84db04187bf625fc433efb1784ed66ceba6dbf0c
Muhammad Haroon <mharoon1326@gmail.com>
2024-10-11 23:09:10 +0500

    init

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 
-void main(){
+void main() {
   runApp(MyApp());
 }
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -12,33 +13,48 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
 class ComplexUI extends StatelessWidget {
   const ComplexUI({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text("Title"),
+        backgroundColor: Colors.teal[200],
+      ),
       body: Column(
         children: <Widget>[
-          AppBar(
-            title: const Text('Hi'),
-            backgroundColor: Colors.teal[200],
-          ),
-          Container(
+          Padding(
+            padding: const EdgeInsets.all(20.0),
             child: Row(
               children: <Widget>[
+                // TextField added here
+                Expanded(
+                  child: TextField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'You can enter anything',
+                    ),
+                  ),
+                ),
+                const SizedBox(width: 20), // Space between TextField and Button
                 ElevatedButton.icon(
                   onPressed: () {
                     print('Button Pressed!');
                   },
                   icon: const Icon(Icons.android_sharp),
                   label: const Text('Android Is The Best!'),
-                )
-              ]
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color.fromARGB(255, 101, 255, 147),
+                  ),
+                ),
+              ],
             ),
-          )
+          ),
         ],
-      )
+      ),
     );
   }
 }
